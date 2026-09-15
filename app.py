71f6eaa257dffd6cdcf9fbddeb0bec81cdb067af
brianchen113 <brianc535@nycstudents.net>
2026-09-15 09:19:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,55 @@ t = Turtle()
 t.shape('turtle')
 
 t.forward(200)
-turtle.done()
+def message(input):
+    print(input)
+message("Good Afternoon")
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.back(90)
+square(200)
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+def equal(x):
+    t.forward(125)
+    t.left(100)
+    t.back(125)
+    t.right(100)
+equal(200)
+
+
+
+
+
+
