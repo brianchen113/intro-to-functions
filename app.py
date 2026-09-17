520fee7c0bb811eeab50b648ee6f9d8d0270d5bf
brianchen113 <brianc535@nycstudents.net>
2026-09-17 09:19:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,28 @@
 import turtle
 from turtle import *
 t = Turtle()
-
 t.shape('turtle')
 
 t.forward(200)
 def message(input):
     print(input)
-message("Good Afternoon")
+message("Hello Class")
+
 def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
     t.left(90)
     t.forward(x)
+    t.forward(x)
     t.left(90)
     t.forward(x)
     t.left(90)
-    t.back(90)
 square(200)
 
-def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200) 
-
-
-def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200) 
-
-def right():
+for i in range(10):
     t.forward(100)
     t.left(90)
-    t.forward(100)
-    t.left(135)
-    t.forward(142)
-right()
-
-def equal(x):
-    t.forward(125)
-    t.left(100)
-    t.back(125)
-    t.right(100)
-equal(200)
-
 
 
 
