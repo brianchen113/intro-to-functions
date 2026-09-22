6d8bee80d6674d81a0c46b1398c8aeca259c200e
brianchen113 <brianc535@nycstudents.net>
2026-09-22 08:58:58 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,24 +1,28 @@
-sidelength = 100
-rotate = 90
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+turtle.done()
+
 def square(x,y):
-    for i in range(4):
+    for i in range(60):
         t.forward(x)
-        t.left(y)\
-        triangle(90,100)
-    
+        t.left(y) # type: ignore
+square(4, 90)
+"""    
 def doubleSquares(iRange):
     length = 25
     for i in range(iRange):
         square(length, 90)
         length = length * 2
 doubleSquares(5)
-
-def addSquares(iRange):
+ """
+""" def addSquares(iRange):
     length = 25
     for i in range(iRange):
         square(length, 90)
         length += 25
-addSquares(5)
+addSquares(5) """
 
 
 
